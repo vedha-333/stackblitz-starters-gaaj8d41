0e7e6751cc0d2bb696a0f7be3af31d049fce809e
Vedant <vedantpandilwad@gmail.com>
2025-01-21 12:06:30 +0530

    api

--- index.js
+++ index.js
@@ -31,21 +31,40 @@
 // Purpose:
 // Help teachers retrieve and analyze student performance efficiently.
 
-
-const express = require('express');
-const { resolve } = require('path');
+const bodyParser = require("body-parser");
+const express = require("express");
+const { resolve } = require("path");
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(bodyParser.json());
+const students = require("./data.json");
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.post("/students/above-threshold", (req, res) => {
+  const { threshold } = req.body;
+
+  if (typeof threshold != "number") {
+    return res
+      .status(400)
+      .json({ error: "Invalid threshold value. It must be a number." });
+  }
+
+  const filteredStudents = students.filter((s) => s.total > threshold);
+
+  const response = {
+    count: filteredStudents.length,
+    students: filteredStudents.map(({ name, total }) => ({ name, total })),
+  };
+  return res.json(response);
+});
+
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
